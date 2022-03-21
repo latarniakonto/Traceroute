240fa5d88201225f18fd1360ea372dd9b2cd1d80
latarniakonto <wisniewskikq27@gmail.com>
2022-03-21 11:35:34 +0100

    Copy template from lecture

--- traceroute.c
+++ traceroute.c
@@ -1,7 +1,64 @@
+#include <netinet/ip.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
 
-int main(int argc, char** argv)
+
+void print_as_bytes (unsigned char* buff, ssize_t length)
 {
-    printf("%s", "Hello World!\n");
-    return 0;
+	for (ssize_t i = 0; i < length; i++, buff++)
+		printf ("%.2x ", *buff);	
+}
+
+u_int16_t compute_icmp_checksum (const void *buff, int length)
+{
+	u_int32_t sum;
+	const u_int16_t* ptr = buff;
+	assert (length % 2 == 0);
+	for (sum = 0; length > 0; length -= 2)
+		sum += *ptr++;
+	sum = (sum >> 16) + (sum & 0xffff);
+	return (u_int16_t)(~(sum + (sum >> 16)));
+}
+
+int main()
+{
+	int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (sockfd < 0) {
+		fprintf(stderr, "socket error: %s\n", strerror(errno)); 
+		return EXIT_FAILURE;
+	}
+
+	for (;;) {
+		
+		struct sockaddr_in 	sender;	
+		socklen_t 			sender_len = sizeof(sender);
+		u_int8_t 			buffer[IP_MAXPACKET];
+
+		ssize_t packet_len = recvfrom (sockfd, buffer, IP_MAXPACKET, 0, (struct sockaddr*)&sender, &sender_len);
+		if (packet_len < 0) {
+			fprintf(stderr, "recvfrom error: %s\n", strerror(errno)); 
+			return EXIT_FAILURE;
+		}
+
+		char sender_ip_str[20]; 
+		inet_ntop(AF_INET, &(sender.sin_addr), sender_ip_str, sizeof(sender_ip_str));
+		printf ("Received IP packet with ICMP content from: %s\n", sender_ip_str);
+
+		struct ip* 			ip_header = (struct ip*) buffer;
+		ssize_t				ip_header_len = 4 * ip_header->ip_hl;
+
+		printf ("IP header: "); 
+		print_as_bytes (buffer, ip_header_len);
+		printf("\n");
+
+		printf ("IP data:   ");
+		print_as_bytes (buffer + ip_header_len, packet_len - ip_header_len);
+		printf("\n\n");
+	}
+
+	return EXIT_SUCCESS;
 }
