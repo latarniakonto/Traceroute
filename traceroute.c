46d280f9be3d4f49f4b6a4754fb2b13f517c1290
latarniakonto <wisniewskikq27@gmail.com>
2022-03-27 18:18:16 +0200

    Remove unnecessary code

--- traceroute.c
+++ traceroute.c
@@ -10,12 +10,6 @@
 #include <sys/time.h>
 
 
-void print_as_bytes (unsigned char* buff, ssize_t length)
-{
-	for (ssize_t i = 0; i < length; i++, buff++)
-		printf ("%.2x ", *buff);
-}
-
 u_int16_t compute_icmp_checksum (const void *buff, int length)
 {
 	u_int32_t sum;
@@ -26,6 +20,7 @@ u_int16_t compute_icmp_checksum (const void *buff, int length)
 	sum = (sum >> 16) + (sum & 0xffff);
 	return (u_int16_t)(~(sum + (sum >> 16)));
 }
+
 
 int valid_IPv4_check(char* ip)
 {
@@ -55,6 +50,8 @@ int valid_IPv4_check(char* ip)
 	free(bytes);
 	return 1;
 }
+
+
 int main (int argc, char** argv)
 {	
 	if (argc == 1) {
@@ -86,8 +83,7 @@ int main (int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 	uint8_t counter = 0;
-	for (int ttl = 1; ttl <= 30; ttl++) {
-		// printf("ttl: %d\n", ttl);
+	for (int ttl = 1; ttl <= 30; ttl++) {		
 		if (setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(int)) < 0) {
 			fprintf(stderr, "setsockopt error: %s\n", strerror(errno));
 			return EXIT_FAILURE;
@@ -133,8 +129,7 @@ int main (int argc, char** argv)
 				fprintf(stderr, "select error: %s\n", strerror(errno));
 				return EXIT_FAILURE;
 			}
-			else if (ready == 0) {
-				// printf("TIMEOUT1\n");
+			else if (ready == 0) {				
 				timeout = 0;
 				break;
 			}
@@ -148,8 +143,7 @@ int main (int argc, char** argv)
 											IP_MAXPACKET,
 											MSG_DONTWAIT,
 											(struct sockaddr*)&sender, &sender_len);
-				if (bytes_recevied < 0) {
-					printf("TIMEOUT2\n");
+				if (bytes_recevied < 0) {					
 					break;
 				}else {
 					char sender_ip_str[20];
@@ -187,13 +181,8 @@ int main (int argc, char** argv)
 						}
 						else {
 							received_ip_addrs[received_packets++] = sender_ip_str;
-						}
-						
-						
-						// printf ("Received IP packet with ICMP content from: %s\n", sender_ip_str);
-						}
-					
-					// printf("%d\n", icmp_header->icmp_type);
+						}																		
+					}									
 				}			
 			}
 			if (received_packets == 3)
@@ -227,44 +216,6 @@ int main (int argc, char** argv)
 				break;
 		}
 	}
-
-    //walidacja argumentu/adresu ip do tracerouta
-    //for(i=1;i<=30;i++)
-    //send_icmp(i);send_icmp(i);send_icmp(i);
-    //recvfrom(socket);recvfrom(socket);recvfrom(socket);
-    //przetwarzanie odpowiedzi z routerów
-    //formatowanie odpowiedzi z routerów
-    //wypisywanie odpowiedzi z routerów
-    //break, gdy otrzymujemy odpowiedź od komputera docelowego
-
-
-	// for (;;) {
-		
-	// 	struct sockaddr_in 	sender;	
-	// 	socklen_t 			sender_len = sizeof(sender);
-	// 	u_int8_t 			buffer[IP_MAXPACKET];
-
-	// 	ssize_t packet_len = recvfrom (sockfd, buffer, IP_MAXPACKET, 0, (struct sockaddr*)&sender, &sender_len);
-	// 	if (packet_len < 0) {
-	// 		fprintf(stderr, "recvfrom error: %s\n", strerror(errno)); 
-	// 		return EXIT_FAILURE;
-	// 	}
-
-	// 	char sender_ip_str[20]; 
-	// 	inet_ntop(AF_INET, &(sender.sin_addr), sender_ip_str, sizeof(sender_ip_str));
-		// printf ("Received IP packet with ICMP content from: %s\n", sender_ip_str);
-
-	// 	struct ip* 			ip_header = (struct ip*) buffer;
-	// 	ssize_t				ip_header_len = 4 * ip_header->ip_hl;
-
-	// 	printf ("IP header: "); 
-	// 	print_as_bytes (buffer, ip_header_len);
-	// 	printf("\n");
-
-	// 	printf ("IP data:   ");
-	// 	print_as_bytes (buffer + ip_header_len, packet_len - ip_header_len);
-	// 	printf("\n\n");
-	// }
 
 	return EXIT_SUCCESS;
 }
